bf4f223327ad7505eecb8e51a1930dda12d5e641
xTumulus <logankitch@gmail.com>
2023-10-06 02:15:43 -0700

    control functions added to BoidSpawner not working

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 [System.Serializable]
 public class BoidSettings {
-  public int velocity = 40;
-  public int boidColliderRadius = 20;
-  public int nearBoidDistance = 10;
-  public int nearObstacleDistance = 20;
-  public int attractPushDist = 1;
+  public int velocity = 32;
+  public int boidColliderRadius = 8;
+  public int nearBoidDistance = 5;
+  public int nearObstacleDistance = 15;
+  public int attractPushDist = 5;
 
   [Header("These \"influences\" are floats, usually from [0...4]")]
   public float velocityMatching = 8f;
   public float flockCentering = 0.2f;
   public float boidAvoid = 4f;
-  public float obstacleAvoid = 500f;
-  public float attractPull = 5f;
+  public float obstacleAvoid = 200f;
+  public float attractPull = 8f;
   public float attractPush = 2f;
 
   [Header("This determines how quickly Boids can turn and is [0...1]")]
-  public float velocityEasing = 0.8f;
+  public float velocityEasing = 0.06f;
   
 }
 
@@ -42,6 +42,69 @@ public class BoidSpawner : MonoBehaviour
     BoidSpawner.SETTINGS = boidSettings;
     BOIDS = new List<Boid>();
     InstantiateBoid();
+  }
+
+  void Update() {
+
+    if (Input.anyKey)
+    {
+     Event e = Event.current;
+      switch (e.keyCode)
+      {
+        case KeyCode.S:
+          //Change Speed (velocity)
+          if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            boidSettings.velocity += 1;
+          }
+          if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            boidSettings.velocity -= 1;
+          }
+          break;
+        case KeyCode.A:
+          //Change Agility (velocityEasing)
+          if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            boidSettings.velocityEasing += 0.01f;
+          }
+          if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            boidSettings.velocityEasing -= 0.01f;
+          }
+          break;
+        case KeyCode.G:
+          //Change Grouping (flockCentering and velocityMatching)
+          if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            boidSettings.flockCentering -= 0.1f;
+          }
+          if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            boidSettings.flockCentering += 0.1f;
+          }
+          // ChangeFloatSetting(ref boidSettings.velocityMatching, 0.5f);
+          // if (Input.GetKeyDown(KeyCode.UpArrow)) {
+          //   setting += delta;
+          // }
+          // if (Input.GetKeyDown(KeyCode.DownArrow)) {
+          //   setting -= delta;
+          // }
+          break;
+        case KeyCode.D: 
+          //Change Flock Density (boidAvoid and nearBoidDistance)
+          if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            boidSettings.boidAvoid -= 1f;
+          }
+          if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            boidSettings.boidAvoid += 1f;
+          }
+          // ChangeIntSetting(ref boidSettings.nearBoidDistance, -1);
+          // if (Input.GetKeyDown(KeyCode.UpArrow)) {
+          //   setting += delta;
+          // }
+          // if (Input.GetKeyDown(KeyCode.DownArrow)) {
+          //   setting -= delta;
+          // }
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   public void InstantiateBoid() {
